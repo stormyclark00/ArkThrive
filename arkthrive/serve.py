a4cbdff9317bfef6cc81d8568b27c99429527b81
stormyclark00 <stormyclark00@gmail.com>
2026-05-07 00:40:57 -0500

    Add files via upload

---
+++ arkthrive/serve.py
@@ -0,0 +1,5 @@
+import os, http.server, socketserver
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", 3456), handler) as httpd:
+    httpd.serve_forever()
